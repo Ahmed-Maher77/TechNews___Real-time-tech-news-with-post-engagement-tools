2d29500e1ba8e90ece4e06ea4af1e7f15a7d736a
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-30 10:19:38 +0300

    Add Home component with associated CSS styles for the main page layout.

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import "./Home.css";
+
+class Home extends Component {
+    render() {
+        return <section className="Home"></section>;
+    }
+}
+
+export default Home;
