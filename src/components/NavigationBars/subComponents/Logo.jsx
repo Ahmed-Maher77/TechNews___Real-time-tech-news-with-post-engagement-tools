c254406784d3cfe2bfefb4e03fdc423ee81f250e
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-30 10:13:45 +0300

    Add Logo component for branding in the navigation bar, featuring an icon and title.

---
+++ src/components/NavigationBars/subComponents/Logo.jsx
@@ -0,0 +1,8 @@
+const Logo = () => (
+    <div className="logo d-flex gap-2 align-items-center p-2 px-3">
+        <i className="fa-solid fa-bezier-curve fs-5"></i>
+        <h1 className="fs-4 lobster-font mb-0">TechNews</h1>
+    </div>
+);
+
+export default Logo;
