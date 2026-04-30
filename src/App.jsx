4c9740d79e4a7acb481be08c6089c5c82a915cef
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-04-30 08:14:22 +0300

    Add main application structure with App component, custom CSS styles, and entry point in main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { Component } from "react";
+import Home from "./pages/Home/Home";
+import CreatePost from "./pages/CreatePost/CreatePost";
+import Sidebar from "./components/NavigationBars/Sidebar/Sidebar";
+import Navbar from "./components/NavigationBars/Navbar/Navbar";
+import Posts from "./pages/Posts/Posts";
+
+class App extends Component {
+    state = {
+        isSmallScreen: window.innerWidth < 992,
+        isSidebarCollapsed: false,
+    };
+
+    handleResize = () => {
+        const isSmallScreen = window.innerWidth < 992;
+        if (isSmallScreen !== this.state.isSmallScreen) {
+            this.setState({ isSmallScreen });
+        }
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.handleResize);
+    }
+
+    render() {
+        const { isSmallScreen, isSidebarCollapsed } = this.state;
+        const pagesClasses = `pages w-100 pb-4 ${isSmallScreen ? "pt-8" : "pt-4"}${
+            !isSmallScreen && isSidebarCollapsed ? " sidebar-collapsed" : ""
+        }`;
+
+        return (
+            <div className="App min-vh-100 d-flex">
+                {isSmallScreen ? (
+                    <Navbar />
+                ) : (
+                    <Sidebar
+                        isCollapsed={isSidebarCollapsed}
+                        onToggleCollapse={() =>
+                            this.setState((prev) => ({
+                                isSidebarCollapsed: !prev.isSidebarCollapsed,
+                            }))
+                        }
+                    />
+                )}
+                <div className={pagesClasses}>
+                    <Home />
+                    <CreatePost />
+                    <Posts />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
